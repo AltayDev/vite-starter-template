175ecbb3b7c602ce08e9190a980ff3395736222a
Oğuzhan <145108492+AltayDev@users.noreply.github.com>
2025-03-22 16:45:18 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <button className="bg-red-600 p-6 m-6 hover:bg-red-800 duration-700 cursor-pointer">
+        Click me!
+      </button>
+    </div>
+  );
+}
+
+export default App;
